2ba8ce03847f5646402d3876a2150f467f7aa054
xiaoge <1142099496@qq.com>
2018-04-29 12:54:28 +0800

    Update js.js

--- static/js.js
+++ static/js.js
@@ -1,11 +1,11 @@
-﻿document.getElementById("menuBox").onclick=function(){slide()};
+document.getElementById("menuBox").onclick=function(){slide()};
 
 function slide(){
 	var box1 = document.getElementById('navBox');
 	var box2 = document.getElementById("menuBox");
 	 if(box1.style.display=="none"){
             box1.style.display="block"; 
-             box2.style.left="340px";
+             box2.style.left="230px";
                              //显示
         }else{
             box1.style.display="none"; 
